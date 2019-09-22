f761166c230ab28f9cb9bc63a76a17657f6db536
lushilling <lushilling@hotmail.co.uk>
2019-09-22 21:00:37 +0100

    set up

---
+++ src/main/java/com/ochida/OchidaApplication.java
@@ -0,0 +1,13 @@
+package com.ochida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OchidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OchidaApplication.class, args);
+	}
+
+}
